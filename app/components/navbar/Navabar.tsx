d81a7be268a6a0e0ba8f13b68e7cda104af56644
michele <michle.carmagnani@gmail.com>
2023-06-08 11:26:19 +0200

    feat: complete navbar ui

--- app/components/navbar/Navabar.tsx
+++ app/components/navbar/Navabar.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Container } from "../Container";
 import { Logo } from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 export const Navabar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0"></div>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
         </Container>
-        <Logo />
       </div>
     </div>
   );
