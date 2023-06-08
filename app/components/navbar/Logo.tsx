d4e80214c75ef2ab4bcfb99a20cdc2368910c40c
michele <michle.carmagnani@gmail.com>
2023-06-08 10:57:27 +0200

    feat: setup and navBar

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      className="cursor-pointer hidden md:block "
+      alt="Logo"
+      height="100"
+      width="100"
+      src="/images/logo.png"
+    />
+  );
+};
+export default Logo;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,tsx,tsx}",
+    "./page/**/*.{js,ts,tsx,tsx}",
+    "./component/**/*.{js,ts,tsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
